d54ed30693004778ad5f9a9f2bf0ec7990a1d955
Yusqual <yusqual@gmail.com>
2025-01-22 00:30:23 +0800

    Site updated: 2025-01-22 00:30:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/leetcode/记录力扣刷题day1.html","/etc/games/speedrun_ext.html"];function toRandomPost(){
+var posts=["/leetcode/lc_day2.html","/etc/games/speedrun_ext.html","/leetcode/记录力扣刷题day1.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
