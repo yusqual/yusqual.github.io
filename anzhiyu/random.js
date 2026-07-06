4983d6714effd802db532d2bdaab86847763bd58
Yusqual <yusqual@gmail.com>
2026-07-06 13:56:12 +0800

    Site updated: 2026-07-06 13:56:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/qt/daily.html","/notes/notes_p1_highprecision.html","/notes/notes_p2_RBTree.html","/leetcode/notes_p4_sort.html","/notes/notes_p3_mutex.html","/games/speedrun_ext.html"];function toRandomPost(){
+var posts=["/qt/daily.html","/notes/notes_p1_highprecision.html","/notes/notes_p2_RBTree.html","/notes/notes_p3_mutex.html","/leetcode/notes_p4_sort.html","/games/speedrun_ext.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
